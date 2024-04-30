40e7820df4fdf69fd8876250cbab3fd7bf5f71b8
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-04-30 10:45:03 -0300

    Tratando erros no formulário de cadastro de especialista

--- src/pages/Cadastro/CadastroEspecialistaTecnico.tsx
+++ src/pages/Cadastro/CadastroEspecialistaTecnico.tsx
@@ -1,7 +1,9 @@
+import { useFieldArray, useForm } from "react-hook-form";
 import {
   Button,
   ButtonContainer,
   Divisor,
+  ErrorMessage,
   Fieldset,
   Form,
   FormContainer,
@@ -9,47 +11,120 @@ import {
   Label,
   Titulo,
 } from "../../components";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"; 
+
+const esquemaCadastroEspecialista = z.object({
+  crm: z.string().min(1, 'Este campo é obrigatório'),
+  especialidades: z.array(z.object({
+    especialidade: z.string().min(1, 'Preencha a sua especialidade.'),
+    anoConclusao: z.number().min(4, 'Preencha o seu ano de conclusão.'),
+    instituicao: z.string().min(1, 'Preencha a instituição de ensino.')
+  }))
+})
+
+type FormEspecialista = z.infer<typeof esquemaCadastroEspecialista>
 
 const CadastroEspecialistaTecnico = () => {
+  const { register, handleSubmit, control, formState: { errors } } = useForm<FormEspecialista>({
+    resolver: zodResolver(esquemaCadastroEspecialista),
+    mode: "all",
+    defaultValues: {
+      crm: "",
+    }
+  });
+
+  const aoSubmeter = (dados: FormEspecialista) => {
+    console.log(dados)
+  };
+
+  const adicionarNovaEspecialidade = () => {
+    append({
+      especialidade: "",
+      anoConclusao: 0,
+      instituicao: ""
+    })
+  }
+
+  const { fields, append } = useFieldArray({
+    control,
+    name: "especialidades"
+  });
+
   return (
     <>
       <Titulo className="titulo">Agora, seus dados técnicos:</Titulo>
-      <Form>
+      <Form onSubmit={handleSubmit(aoSubmeter)}>
         <Fieldset>
           <Label>CRM</Label>
           <Input
             id="campo-crm"
             type="text"
             placeholder="Insira seu número de registro"
+            $error={!!errors.crm}
+            {...register("crm")}
           />
+          {errors.crm && (
+            <ErrorMessage>{errors.crm.message}</ErrorMessage>
+          )}
         </Fieldset>
         <Divisor />
-        <Fieldset>
-          <Label>Especialidade</Label>
-          <Input
-            id="campo-especialidade"
-            type="text"
-            placeholder="Qual sua especialidade?"
-          />
-        </Fieldset>
+        {fields.map((field, index) => (
+          <div key={field.id}>
+            <Fieldset>
+              <Label>Especialidade</Label>
+              <Input
+                id="campo-especialidade"
+                type="text"
+                placeholder="Qual sua especialidade?"
+                $error={!!errors.especialidades?.[index]?.especialidade}
+                {...register(`especialidades.${index}.especialidade`)}
+              />
+              {errors.especialidades?.[index]?.especialidade && (
+                <ErrorMessage>
+                  {errors.especialidades?.[index]?.especialidade?.message}
+                </ErrorMessage>
+              )}
+            </Fieldset>
 
-        <FormContainer>
-          <Fieldset>
-            <Label>Ano de conclusão</Label>
-            <Input id="campo-ano-conclusao" type="text" placeholder="2005" />
-          </Fieldset>
-          <Fieldset>
-            <Label>Instituição de ensino</Label>
-            <Input
-              id="campo-instituicao-ensino"
-              type="text"
-              placeholder="USP"
-            />
-          </Fieldset>
-        </FormContainer>
-        <Divisor />
+            <FormContainer>
+              <Fieldset>
+                <Label>Ano de conclusão</Label>
+                <Input 
+                  id="campo-ano-conclusao" 
+                  type="text" 
+                  placeholder="2005"
+                  $error={!!errors.especialidades?.[index]?.anoConclusao}
+                  {...register(`especialidades.${index}.anoConclusao`)}
+                />
+                {errors.especialidades?.[index]?.anoConclusao && (
+                    <ErrorMessage>
+                      {errors.especialidades?.[index]?.anoConclusao?.message}
+                    </ErrorMessage>
+                  )}
+              </Fieldset>
+              <Fieldset>
+                <Label>Instituição de ensino</Label>
+                <Input
+                  id="campo-instituicao-ensino"
+                  type="text"
+                  placeholder="USP"
+                  $error={!!errors.especialidades?.[index]?.instituicao}
+                  {...register(`especialidades.${index}.instituicao`)}
+                />
+                {errors.especialidades?.[index]?.instituicao && (
+                    <ErrorMessage>
+                      {errors.especialidades?.[index]?.instituicao?.message}
+                    </ErrorMessage>
+                )}
+              </Fieldset>
+            </FormContainer>
+            <Divisor />
+          </div>
+        ))}
+
         <ButtonContainer>
-          <Button type="button" $variante="secundario">
+          <Button type="button" onClick={adicionarNovaEspecialidade} $variante="secundario">
             Adicionar Especialidade
           </Button>
         </ButtonContainer>
--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,10 +1,10 @@
 import { Logotipo } from "../../components";
-import CadastroPessoal from "./CadastroPessoal";
+import CadastroEspecialistaTecnico from "./CadastroEspecialistaTecnico";
 export default function Cadastro() {
   return (
     <>
       <Logotipo />
-      <CadastroPessoal />
+      <CadastroEspecialistaTecnico />
     </>
   );
 }
